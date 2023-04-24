cf118e0d31657483b9acae73a91eb0cecd2a2036
ibro <sameoldlab@proton.me>
2023-04-24 10:55:30 +0100

    filters include multiple authors

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,22 +1,19 @@
 import type { PageLoad } from "./$types"
 import { agent } from "$lib/atsession"
+import { error } from "@sveltejs/kit";
 
 export const load = (async () => {
-  // Validate session
-  if (!agent.hasSession) return
-
  /*  const follows = async () => {
     const { success, data } = await agent.getFollows({actor: })
     if (success) return data.follows
   } */
-  const feed = async () => {
-    const res = await agent.getTimeline({ limit: 50 })
-    if (res.success) {
-        return res.data.feed
-    } else {
-        throw res.success
-    }
-  }
+  const feed = () => agent.getTimeline({ limit: 50 })
+    .then(res => {
+        if(!res.success) throw error(402, 'fdfaf')
+        return {...res.data}
+    })
+    .catch(err => console.error("failed to retreive timeline", err));
+  
   return {
     // follows: follows(),
     feed: feed()
